33f7989f3f860a92963e9958c2c5aa827123721b
Jean-Michel Williams <jeanmichelwilliams3@gmail.com>
2025-10-02 12:24:49 -0400

    6tss 1111

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {\n    port: 5174,\n    proxy: {\n      \'/api/auth\': {\n        target: \'https://my-backend-worker.jeanmichelwilliams3.workers.dev\',\n        changeOrigin: true,\n        rewrite: (path) => path.replace(/^\/api\/auth/, \'/api/auth\'),\n      },\n    },\n  },\n})
+  server: {
+    port: 5174,
+  },
+})
